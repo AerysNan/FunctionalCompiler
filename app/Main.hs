e4557b77d594aa8c7ca8b9ef650d497c0e141f65
喻琳颖 <994646643@qq.com>
2019-06-13 22:50:11 +0800

    bugfix

--- app/Main.hs
+++ app/Main.hs
@@ -11,38 +11,38 @@ import Debug.Trace
 main :: IO ()
 main = do
   hSetBuffering stdout NoBuffering
-  repl (TypeContext empty empty) empty
+  repl (ADTContext [] empty) (TypeContext empty empty) empty
 
-repl :: TypeContext -> ValueContext -> IO ()
-repl tCtx vCtx = do
+repl :: ADTContext -> TypeContext -> ValueContext -> IO ()
+repl ctx tCtx vCtx = do
   putStr "> "
   inputString <- getLine
   if inputString == "quit"
     then return ()
     else
-      let inputExpr = inputParser inputString in
+      let (inputExpr, adtCtx) = inputParser ctx inputString in
         case inputExpr of
           (Left errorMsg) -> do
             putStrLn errorMsg
-            repl tCtx vCtx
+            repl adtCtx tCtx vCtx
           (Right (Single expr)) ->
             case eval expr tCtx vCtx of
               Nothing -> do
                 putStrLn "Invalid expression!"
-                repl tCtx vCtx
+                repl adtCtx tCtx vCtx
               (Just (t, v)) -> do
                 printValue v
-                repl tCtx vCtx
+                repl adtCtx tCtx vCtx
           (Right (Assign name expr)) ->
             case eval expr tCtx vCtx of
               Nothing -> do
                 putStrLn "Invalid expression!"
-                repl tCtx vCtx
+                repl adtCtx tCtx vCtx
               (Just (t, v)) -> do
                 printValue v
-                repl (tCtx {getVars = insert name t $ getVars tCtx}) (insert name v vCtx)
+                repl adtCtx (tCtx {getVars = insert name t $ getVars tCtx}) (insert name v vCtx)
           (Right (Class adt)) ->
-            repl (tCtx {getCtors = union (EvalType.getADTCtors [adt]) $ getCtors tCtx}) (union (getADTs [adt]) vCtx)
+            repl adtCtx (tCtx {getCtors = union (EvalType.getADTCtors [adt]) $ getCtors tCtx}) (union (getADTs [adt]) vCtx)
 
 eval :: Expr -> TypeContext -> ValueContext -> Maybe (Type, Value)
 eval expr tCtx vCtx =
