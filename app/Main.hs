0c2be18ab73ceeacaa27d7dafa3bef1d3523ae82
AerysNan <aerysnan@gmail.com>
2019-06-14 12:34:11 +0800

    enable multiline statement & script

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,11 @@ import Parser
 import EvalType
 import EvalValue
 import System.IO
-import Data.Map
+import Data.Map (insert, union, empty)
+import Data.List (dropWhile, dropWhileEnd)
 import Debug.Trace
+import Data.List.Split (wordsBy)
+import Control.Monad
 
 main :: IO ()
 main = do
@@ -14,35 +17,75 @@ main = do
   repl (ADTContext [] empty) (TypeContext empty empty) empty
 
 repl :: ADTContext -> TypeContext -> ValueContext -> IO ()
-repl ctx tCtx vCtx = do
+repl adtCtx tCtx vCtx = do
   putStr "> "
   inputString <- getLine
-  if inputString == "quit"
-    then return ()
-    else
-      let (inputExpr, adtCtx) = inputParser ctx inputString in
-        case inputExpr of
-          (Left errorMsg) -> do
-            putStrLn errorMsg
-            repl adtCtx tCtx vCtx
-          (Right (Single expr)) ->
-            case eval expr tCtx vCtx of
-              Nothing -> do
-                putStrLn "Invalid expression!"
-                repl adtCtx tCtx vCtx
-              (Just (t, v)) -> do
-                printValue v
-                repl adtCtx tCtx vCtx
-          (Right (Assign name expr)) ->
-            case eval expr tCtx vCtx of
-              Nothing -> do
-                putStrLn "Invalid expression!"
-                repl adtCtx tCtx vCtx
-              (Just (t, v)) -> do
-                printValue v
-                repl adtCtx (tCtx {getVars = insert name t $ getVars tCtx}) (insert name v vCtx)
-          (Right (Class adt)) ->
-            repl adtCtx (tCtx {getCtors = union (EvalType.getADTCtors [adt]) $ getCtors tCtx}) (union (getADTs [adt]) vCtx)
+  case trim inputString of
+    ":q" -> return ()
+    ":{" -> do
+      block <- readBlock ""
+      deal block adtCtx tCtx vCtx
+    _ -> let words = wordsBy (== ' ') inputString in
+      if length words == 2 && head words == ":l"
+        then do
+          content <- readFile $ last words
+          deal content adtCtx tCtx vCtx
+        else
+          deal inputString adtCtx tCtx vCtx
+
+readBlock :: String -> IO String
+readBlock acc = do
+  putStr "| "
+  line <- getLine
+  if trim line == ":}"
+    then return acc
+    else readBlock $ acc ++ line
+
+
+trim :: String -> String
+trim = dropWhileEnd (' ' ==) . dropWhile (' ' ==)
+
+deal :: String -> ADTContext -> TypeContext -> ValueContext -> IO ()
+deal inputString adtCtx tCtx vCtx =
+  let (inputStmts, newCtx) = inputParser adtCtx inputString in
+    case inputStmts of
+      (Left errorMsg) -> do
+        putStrLn errorMsg
+        repl newCtx tCtx vCtx
+      (Right [stmt]) -> do
+        (newTCtx, newVCtx) <- dealStatement stmt True tCtx vCtx
+        repl newCtx newTCtx newVCtx
+      (Right stmts) -> do
+        (newTCtx, newVCtx) <- dealStatements stmts tCtx vCtx
+        repl newCtx newTCtx newVCtx
+
+dealStatements :: [Statement] -> TypeContext -> ValueContext -> IO (TypeContext, ValueContext)
+dealStatements [] tCtx vCtx = return (tCtx, vCtx)
+dealStatements (stmt : stmts) tCtx vCtx = do
+  (newTCtx, newVCtx) <- dealStatement stmt False tCtx vCtx
+  dealStatements stmts newTCtx newVCtx
+
+dealStatement :: Statement -> Bool -> TypeContext -> ValueContext -> IO (TypeContext, ValueContext)
+dealStatement statement verbose tCtx vCtx =
+  case statement of
+    (Single expr) ->
+      case eval expr tCtx vCtx of
+        Nothing -> do
+          putStrLn "Invalid expression!"
+          return (tCtx, vCtx)
+        (Just (t, v)) -> do
+          when verbose $ printValue v
+          return (tCtx, vCtx)
+    (Assign name expr) ->
+      case eval expr tCtx vCtx of
+        Nothing -> do
+          putStrLn "Invalid expression!"
+          return (tCtx, vCtx)
+        (Just (t, v)) -> do
+          when verbose $ printValue v
+          return (tCtx {getVars = insert name t $ getVars tCtx}, insert name v vCtx)
+    (Class adt) ->
+      return (tCtx {getCtors = union (EvalType.getADTCtors [adt]) $ getCtors tCtx}, getADTs [adt] `union` vCtx)
 
 eval :: Expr -> TypeContext -> ValueContext -> Maybe (Type, Value)
 eval expr tCtx vCtx =
@@ -59,7 +102,7 @@ toLiteral :: Value -> Maybe String
 toLiteral v = case v of
   (VBool vb) -> Just $ show vb
   (VInt vi) -> Just $ show vi
-  (VChar vc) -> Just$ show vc
+  (VChar vc) -> Just $ show vc
   (VAdt name values) -> (++) <$> Just name <*> fmap concat (mapM (fmap ((:) ' ') . toLiteral) values)
   _ -> Nothing
 
